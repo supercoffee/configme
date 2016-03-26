36a244d397a51773044cc2a36b6f3c66c7480e7d
Benjamin Daschel <coffeemaxed@gmail.com>
2016-03-26 15:03:03 -0700

    render template successfully

--- configme.py
+++ configme.py
@@ -1,20 +1,32 @@
 __author__ = 'ben'
 
+from jinja2 import Environment, FileSystemLoader, Template
+from os import getcwd, path, walk
 import sys
 from termcolor import colored
 import yaml
 
 
+def load_config(environment):
+    filename = '{}.yml'.format(environment)
+    try:
+        with open(filename, 'r') as configfile:
+            return yaml.load(configfile)
+    except IOError:
+        exit(colored('Config file {} does not exist'.format(filename), 'red'))
+
 class ConfigMe():
 
-    def deploy(self, environment):
-        filename = '{}.yml'.format(environment)
-        try:
-            with open(filename, 'r') as configfile:
-                config = yaml.load(configfile)
-        except IOError:
-            exit(colored('Config file {} does not exist'.format(filename), 'red'))
 
+    def deploy(self, environment):
+        config = load_config(environment)
+        template_paths = path.join(getcwd(), 'templates')
+        env = Environment(loader=FileSystemLoader(template_paths))
+        for template_name in env.list_templates('.j2'):
+            template = env.get_template(template_name)
+            print 'Rendering template: {}'.format(template_name)
+            output = template.render(config)
+            print output
 
 def main():
     args = sys.argv
