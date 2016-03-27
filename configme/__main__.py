ac8911dcaff9ed65969e278a36f42db9a999ed33
Benjamin Daschel <coffeemaxed@gmail.com>
2016-03-26 17:51:22 -0700

    fix argpare derp

--- configme/__main__.py
+++ configme/__main__.py
@@ -4,9 +4,9 @@ from configme import load_config, make_args, ConfigMe
 
 def main():
     args = make_args()
-    config = load_config(args['config'])
-    cfg_manager = ConfigMe(config, args['projectpath'])
-    method = getattr(cfg_manager, args['command'])
+    config = load_config(args.config)
+    cfg_manager = ConfigMe(config, args.projectpath)
+    method = getattr(cfg_manager, args.command)
     try:
         if method is not None:
             method()
